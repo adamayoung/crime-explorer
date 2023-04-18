851fd5538bc8cce2c952850d32e9d36631049d74
Adam Young <me@adam-young.co.uk>
2023-04-18 22:16:21 +0100

    Initial commit

---
+++ CrimeExplorer/CrimeExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeExplorerApp.swift
+//  CrimeExplorer
+//
+//  Created by Adam Young on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrimeExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
